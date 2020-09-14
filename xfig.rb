888e458cb62c096db522d73a141837c5aee489fd
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2020-09-14 11:47:06 +1000

    fix sha256

--- xfig.rb
+++ xfig.rb
@@ -2,7 +2,7 @@ class Xfig < Formula
   desc "Interactive drawing tool for X"
   homepage "https://mcj.sourceforge.io"
   url "https://downloads.sourceforge.net/projects/mcj/xfig-full-3.2.7.tar.xz"
-  sha256 "123617d6e751d381853242399ebfa5a61a36565f"
+  sha256 "e6fac2f76e34edbd6595069783379775598f3aa5b0a43d7a97353aa314c0482f"
 
   depends_on "fig2dev"
   depends_on "ghostscript"
